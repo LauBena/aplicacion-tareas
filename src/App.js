b8ae6679d400604d8b86a0262ee86d284e23c35b
LauBena <87872842+LauBena@users.noreply.github.com>
2022-07-16 15:49:28 -0300

    realizo cambios en app js y css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import ListaTareas from './componentes/ListaTareas';
+
 
 function App() {
   return (
     <div className="App-tareas">
-      React
+      <div className="lista-principal">
+        <h1>Mis Tareas</h1>
+        <ListaTareas />
+      </div>
     </div>
   );
 }
